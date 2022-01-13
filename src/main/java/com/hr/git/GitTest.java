17818da9819e81fbe622b07c575663ea8f74e92c
wxp <1544725630@qq.com>
2022-01-13 15:21:54 +0800

    thread commit

--- src/main/java/com/hr/git/GitTest.java
+++ src/main/java/com/hr/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git----1.1");
+        System.out.println("hello git----1.2");
     }
 }
