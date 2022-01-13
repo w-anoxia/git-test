5b2808773f3ca0047459f1a3533d9dbb5ac908ba
wxp <1544725630@qq.com>
2022-01-13 15:20:57 +0800

    second commit

--- src/main/java/com/hr/git/GitTest.java
+++ src/main/java/com/hr/git/GitTest.java
@@ -7,5 +7,6 @@ package com.hr.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git----1.1");
     }
 }
