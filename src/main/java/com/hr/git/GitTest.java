d99d3ed2cbd92f36a92c3213b4b99b01f0f8318b
wxp <1544725630@qq.com>
2022-01-13 15:18:37 +0800

    first commit

---
+++ src/main/java/com/hr/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hr.git;
+
+/**
+ * @author wxp
+ * @date 2022/1/13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
