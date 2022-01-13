4e61adbdc0a9233a21eba9d6b181b2f59838ca47
wxp <1544725630@qq.com>
2022-01-13 15:28:35 +0800

    hot-fix commit

--- src/main/java/com/hr/git/GitTest.java
+++ src/main/java/com/hr/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git----1.1");
         System.out.println("hello git----1.2");
+        System.out.println("hello git----1.3");
     }
 }
